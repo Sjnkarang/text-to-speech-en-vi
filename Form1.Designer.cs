5eaa97b07ed845f40f526cb3f39ae5c1ed7b5c33
Tran Nguyen Sinh <69780117+Sjnkarang@users.noreply.github.com>
2020-12-05 23:10:56 +0700

    code only

---
+++ Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace Docso
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.viBtn = new System.Windows.Forms.RadioButton();
+            this.enBtn = new System.Windows.Forms.RadioButton();
+            this.input = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.output = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // viBtn
+            // 
+            this.viBtn.AutoSize = true;
+            this.viBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.viBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.viBtn.Location = new System.Drawing.Point(385, 117);
+            this.viBtn.Name = "viBtn";
+            this.viBtn.Size = new System.Drawing.Size(44, 28);
+            this.viBtn.TabIndex = 0;
+            this.viBtn.TabStop = true;
+            this.viBtn.Text = "VI";
+            this.viBtn.UseVisualStyleBackColor = true;
+            this.viBtn.CheckedChanged += new System.EventHandler(this.viBtn_CheckedChanged);
+            // 
+            // enBtn
+            // 
+            this.enBtn.AutoSize = true;
+            this.enBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.enBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.enBtn.Location = new System.Drawing.Point(486, 117);
+            this.enBtn.Name = "enBtn";
+            this.enBtn.Size = new System.Drawing.Size(54, 28);
+            this.enBtn.TabIndex = 1;
+            this.enBtn.TabStop = true;
+            this.enBtn.Text = "EN";
+            this.enBtn.UseVisualStyleBackColor = true;
+            this.enBtn.CheckedChanged += new System.EventHandler(this.enBtn_CheckedChanged);
+            // 
+            // input
+            // 
+            this.input.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.input.Location = new System.Drawing.Point(432, 178);
+            this.input.Name = "input";
+            this.input.Size = new System.Drawing.Size(399, 29);
+            this.input.TabIndex = 2;
+            this.input.TextChanged += new System.EventHandler(this.input_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(83, 181);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(169, 24);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Số cần chuyển đổi";
+            // 
+            // output
+            // 
+            this.output.BackColor = System.Drawing.SystemColors.Control;
+            this.output.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.output.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.output.Location = new System.Drawing.Point(432, 241);
+            this.output.Name = "output";
+            this.output.Size = new System.Drawing.Size(399, 138);
+            this.output.TabIndex = 4;
+            this.output.Text = "";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(83, 241);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(221, 24);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Chữ đã được chuyển đổi";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(231, 46);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(483, 37);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Chương trình đọc từ số sang chữ";
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(320, 429);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(266, 70);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Đọc thành tiếng";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(948, 573);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.output);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.input);
+            this.Controls.Add(this.enBtn);
+            this.Controls.Add(this.viBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton viBtn;
+        private System.Windows.Forms.RadioButton enBtn;
+        private System.Windows.Forms.TextBox input;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox output;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,333 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+using System.Globalization;
+//voice english
+using System.Speech.Synthesis;
+
+//voice vietnamese
+using System.Net.Http;
+
+using Google.Cloud.TextToSpeech.V1;
+using Google.Apis.Auth.OAuth2;
+using Google.Protobuf;
+
+using System.Net;
+
+
+
+
+namespace Docso
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            string cur_path = Path.Combine(Directory.GetCurrentDirectory(), "key.json");
+            Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", cur_path, EnvironmentVariableTarget.Process);
+            InitializeComponent();
+        }
+
+        string[] mNumText = "không;một;hai;ba;bốn;năm;sáu;bảy;tám;chín".Split(';');
+
+        private string DocHangChuc(double so, bool daydu)
+        {
+            string chuoi = "";
+            Int64 chuc = Convert.ToInt64(Math.Floor((double)(so / 10)));
+            Int64 donvi = (Int64)so % 10;
+            if (chuc > 1)
+            {
+                chuoi = " " + mNumText[chuc] + " mươi";
+                if (donvi == 1)
+                {
+                    chuoi += " mốt";
+                }
+            }
+            else if (chuc == 1)
+            {
+                chuoi = " mười";
+                if (donvi == 1)
+                {
+                    chuoi += " một";
+                }
+            }
+            else if (daydu && donvi > 0)
+            {
+                chuoi = " lẻ";
+            }
+            if (donvi == 5 && chuc >= 1)
+            {
+                chuoi += " lăm";
+            }
+            else if (donvi > 1 || (donvi == 1 && chuc == 0))
+            {
+                chuoi += " " + mNumText[donvi];
+            }
+            return chuoi;
+        }
+        private string DocHangTram(double so, bool daydu)
+        {
+            string chuoi = "";
+            Int64 tram = Convert.ToInt64(Math.Floor((double)so / 100));
+            so = so % 100;
+            if (daydu || tram > 0)
+            {
+                chuoi = " " + mNumText[tram] + " trăm";
+                chuoi += DocHangChuc(so, true);
+            }
+            else
+            {
+                chuoi = DocHangChuc(so, false);
+            }
+            return chuoi;
+        }
+        private string DocHangTrieu(double so, bool daydu)
+        {
+            string chuoi = "";
+            Int64 trieu = Convert.ToInt64(Math.Floor((double)so / 1000000));
+            so = so % 1000000;
+            if (trieu > 0)
+            {
+                chuoi = DocHangTram(trieu, daydu) + " triệu";
+                daydu = true;
+            }
+            //Lấy số hàng nghìn
+            Int64 nghin = Convert.ToInt64(Math.Floor((double)so / 1000));
+            //Lấy phần dư sau số hàng nghin 
+            so = so % 1000;
+            if (nghin > 0)
+            {
+                chuoi += DocHangTram(nghin, daydu) + " nghìn";
+                daydu = true;
+            }
+            if (so > 0)
+            {
+                chuoi += DocHangTram(so, daydu);
+            }
+            return chuoi;
+        }
+
+        public string ChuyenSoSangChuoi(double so)
+        {
+            if (so == 0)
+                return mNumText[0];
+            string chuoi = "", hauto = "";
+            Int64 ty;
+            do
+            {
+                ty = Convert.ToInt64(Math.Floor((double)so / 1000000000));
+                so = so % 1000000000;
+                if (ty > 0)
+                {
+                    chuoi = DocHangTrieu(so, true) + hauto + chuoi;
+                }
+                else
+                {
+                    chuoi = DocHangTrieu(so, false) + hauto + chuoi;
+                }
+                hauto = " tỷ";
+            } while (ty > 0);
+            
+            if (chuoi[0] == ' ')
+            {
+                chuoi = chuoi.Substring(1);
+            }
+
+            string res = char.ToUpper(chuoi[0]) + chuoi.Substring(1);
+
+            return res;
+        }
+    
+        public static string NumberToWords(long number)
+        {
+            if (number == 0)
+                return "Zero";
+
+            if (number < 0)
+                return "minus " + NumberToWords(Math.Abs(number));
+
+            string words = "";
+
+            if ((number / 1000000000000) > 0)
+            {
+                words += NumberToWords(number / 1000000000000) + " trillion ";
+                number %= 1000000000000;
+            }
+
+            if ((number / 1000000000) > 0)
+            {
+                words += NumberToWords(number / 1000000000) + " billion ";
+                number %= 1000000000;
+            }
+
+            if ((number / 1000000) > 0)
+            {
+                words += NumberToWords(number / 1000000) + " million ";
+                number %= 1000000;
+            }
+
+            if ((number / 1000) > 0)
+            {
+                words += NumberToWords(number / 1000) + " thousand ";
+                number %= 1000;
+            }
+
+            if ((number / 100) > 0)
+            {
+                words += NumberToWords(number / 100) + " hundred ";
+                number %= 100;
+            }
+
+            if (number > 0)
+            {
+                if (words != "")
+                    words += "and ";
+
+                var unitsMap = new[] { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen" };
+                var tensMap = new[] { "zero", "one", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety" };
+
+                if (number < 20)
+                    words += unitsMap[number];
+                else
+                {
+                    words += tensMap[number / 10];
+                    if ((number % 10) > 0)
+                        words += " " + unitsMap[number % 10];
+                }
+            }
+
+            string res = words;
+
+            return res;
+        }
+
+        string xuly(string x)
+        {
+            
+            var str = x;
+            var charsToRemove = new string[] {","};
+            foreach (var c in charsToRemove)
+            {
+                str = str.Replace(c, string.Empty);
+            }
+
+            return str;
+
+        }
+
+        private void viBtn_CheckedChanged(object sender, EventArgs e)
+        {
+            input_TextChanged(sender, e);
+        }
+
+        private void enBtn_CheckedChanged(object sender, EventArgs e)
+        {
+            input_TextChanged(sender, e);
+        }
+
+        private void input_TextChanged(object sender, EventArgs e)
+        {
+            string inpTemp = input.Text;
+
+            string inpStr = xuly(inpTemp);
+
+            long inp;
+            bool success = Int64.TryParse(inpStr, out inp);
+
+            if (input.Text != "")
+            {
+                if (success)
+                {
+                    input.Text = inp.ToString("#,##0");
+                    input.Select(input.Text.Length, 0);
+                    if (viBtn.Checked == true)
+                    {
+                        if (inp > 1000000000)
+                            output.Text = "Vui lòng nhập số bé hơn 1 tỷ";
+                        else output.Text = ChuyenSoSangChuoi(inp);
+                    }
+                    else
+                    {
+                        string outtttt = NumberToWords(inp);
+                        string ress = char.ToUpper(outtttt[0]) + outtttt.Substring(1);
+
+                        output.Text = ress;
+                    }
+                }
+                else
+                {
+
+                    if (inpStr.Length > 18)
+                        output.Text = "Quá giới hạn - Out of range";
+                    else 
+                        output.Text = "Vui lòng không nhập chữ";
+
+                }
+            }
+            else
+            {
+                output.Text = "";
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string inpTemp = input.Text;
+
+            string inpStr = xuly(inpTemp);
+
+            long inp;
+            bool success = Int64.TryParse(inpStr, out inp);
+
+
+            if (viBtn.Checked == true)
+            {
+                String payload = ChuyenSoSangChuoi(inp);
+
+
+                TextToSpeechClient client = TextToSpeechClient.Create();
+
+                SynthesizeSpeechResponse response = client.SynthesizeSpeech(
+                    new SynthesisInput()
+                    {
+                        Text = payload
+                    },
+                    new VoiceSelectionParams()
+                    {
+                        LanguageCode = "vi-VN",
+                        Name = "vi-VN-Standard-A"
+                    },
+                    new AudioConfig()
+                    {
+                        AudioEncoding = AudioEncoding.Linear16
+                    }
+                );
+
+                string speechFile = Path.Combine(Directory.GetCurrentDirectory(), "sample.wav");
+
+                File.WriteAllBytes(speechFile, response.AudioContent.ToByteArray());
+
+                
+                System.Media.SoundPlayer player = new System.Media.SoundPlayer(speechFile);
+                player.Play();
+
+            }
+            else
+            {
+                var synthesizer = new SpeechSynthesizer();
+                synthesizer.SetOutputToDefaultAudioDevice();
+                synthesizer.Speak(NumberToWords(inp));
+            }
+        }
+    }
+}
